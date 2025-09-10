35c9302dbe9e9ca943c20569217309b366cb4c31
rajra <rajra@DESKTOP-J5LOURV.lan>
2025-09-10 12:03:55 -0500

    added logger

--- springassessment/src/main/java/com/example/demo/controller/Rewards.java
+++ springassessment/src/main/java/com/example/demo/controller/Rewards.java
@@ -2,6 +2,8 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +20,8 @@ import com.example.demo.service.CalculateRewards;
 @RequestMapping("/api")
 public class Rewards {
 	
+	private static final Logger logger = LoggerFactory.getLogger(Rewards.class);
+	
 	@Autowired
 	public CalculateRewards calReward;
 
@@ -25,7 +29,7 @@ public class Rewards {
 	public ResponseEntity<List<RewardResponseBody>> getRewards() {
 		
 		List<RewardResponseBody> resp = calReward.getRewards();
-		
+		logger.info("Reward List are: ", resp);
 		return new ResponseEntity<List<RewardResponseBody>>(resp, HttpStatus.OK); 
 	}
 	
@@ -33,7 +37,7 @@ public class Rewards {
 	public ResponseEntity<List<Reward>> getRewardsOfCustId(@PathVariable int customerId) {
 		
 		ResponseEntity<List<Reward>> resp = calReward.getRewardsDetailsOfCustId(customerId);
-		
+		logger.info("Reward of the customer of customerid {} is: {}", customerId, resp);
 		return resp;
 	}
 }
